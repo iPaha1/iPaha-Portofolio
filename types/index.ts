87e9234798081eded78a93df88e3d6f762f504fa
iPaha1 <ike4football@gmail.com>
2024-01-18 03:13:40 +0000

    deployment error fixed

--- types/index.ts
+++ types/index.ts
@@ -67,4 +67,19 @@ export type CreateUserParams = {
     updatedAt: Date
   }
 
+  // ====== URL QUERY PARAMS
+  export type UrlQueryParams = {
+    params: string
+    key: string
+    value: string | null
+  }
   
+  export type RemoveUrlQueryParams = {
+    params: string
+    keysToRemove: string[]
+  }
+  
+  export type SearchParamProps = {
+    params: { id: string }
+    searchParams: { [key: string]: string | string[] | undefined }
+  }
