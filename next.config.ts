5ae87058f4bcb980c4b5dd4da2ec240e2a44684b
iPaha1 <ike4football@gmail.com>
2026-03-26 22:11:31 +0000

    Fix: fix vercel deployment error

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   images: {
-    domains: ["res.cloudinary.com", "img.clerk.com", "via.placeholder.com"],
+    // domains: ["res.cloudinary.com", "img.clerk.com", "via.placeholder.com"],
     deviceSizes:  [640, 750, 828, 1080, 1200, 1920],
     imageSizes:   [16, 32, 48, 64, 96, 128, 256, 384],
     formats:      ["image/avif", "image/webp"],
